d7f21cecb4f14040a623beae45e610e95d6eb6b7
MeridjaNassim <ha_meridja@esi.dz>
2020-09-17 20:23:03 +0100

    Added first 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,14 +2,32 @@ import { PageProps } from "gatsby"
 import React from "react"
 import styled from "styled-components"
 import { H1 } from "../components/typography/typography"
+import cls from "classnames"
 export default function (props: PageProps) {
   return (
-    <div className="flex justify-center items-center h-full w-full">
-      <Title>404 | Page not found</Title>
-    </div>
+    <Wrapper>
+      <Title>
+        <span className="text-blue-500 font-medium">404</span>
+        <VerticalLine className="bg-blue-500"></VerticalLine>
+        <span>Page not found</span>
+      </Title>
+    </Wrapper>
   )
 }
 
-const Title = styled(H1).attrs({
-  className: "font-light",
-})``
+/// Styled Components
+const Wrapper = styled.div.attrs((props) => ({
+  className: cls("flex justify-center items-center"),
+}))`
+  height: 100vh;
+  width: 100%;
+`
+const VerticalLine = styled.span.attrs((props) => ({
+  className: cls("inline-block mx-5 bg-black rounded-sm", props.className),
+}))`
+  height: 3rem;
+  width: 5px;
+`
+const Title = styled(H1).attrs((props) => ({
+  className: cls("font-thin"),
+}))``
