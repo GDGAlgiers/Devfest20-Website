0735982f48f3c037e4dbe178f5ab3ad10249490d
MeridjaNassim <ha_meridja@esi.dz>
2020-09-21 19:16:49 +0100

    Added additional props to Typography Elements

--- src/components/typography/typography.tsx
+++ src/components/typography/typography.tsx
@@ -1,20 +1,26 @@
 import styled from "styled-components"
-
-export const H1 = styled.h1.attrs({
-  className: "c-h1",
-})``
-export const H2 = styled.h2.attrs({
-  className: "c-h2",
-})``
-export const H3 = styled.h3.attrs({
-  className: "c-h3",
-})``
-export const H4 = styled.h4.attrs({
-  className: "c-h4",
-})``
-export const H5 = styled.h5.attrs({
-  className: "c-h5",
-})``
-export const H6 = styled.h6.attrs({
-  className: "c-h6",
-})``
+import cls from "classnames"
+export const H1 = styled.h1.attrs((props) => ({
+  ...props,
+  className: cls("c-h1", props.className),
+}))``
+export const H2 = styled.h2.attrs((props) => ({
+  ...props,
+  className: cls("c-h2", props.className),
+}))``
+export const H3 = styled.h3.attrs((props) => ({
+  ...props,
+  className: cls("c-h3", props.className),
+}))``
+export const H4 = styled.h4.attrs((props) => ({
+  ...props,
+  className: cls("c-h4", props.className),
+}))``
+export const H5 = styled.h5.attrs((props) => ({
+  ...props,
+  className: cls("c-h5", props.className),
+}))``
+export const H6 = styled.h6.attrs((props) => ({
+  ...props,
+  className: cls("c-h6", props.className),
+}))``
