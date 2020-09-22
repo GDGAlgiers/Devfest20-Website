8b3cf10092156e9c5b14a7931847ad749f90b39e
MeridjaNassim <ha_meridja@esi.dz>
2020-09-22 20:44:45 +0100

    Added NavBar UI

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,70 @@
-import React, { ReactElement } from "react"
-import { useMediaQuery } from "react-responsive"
-import { useAtleastSmall, useAtleastMedium } from "../../utils/medias"
-interface Props {}
+import React, { ReactElement, useEffect, useState } from "react"
+import styled from "styled-components"
+import cls from "classnames"
+import { useAtleastSmall } from "../../utils/medias"
 
-function Navbar({}: Props): ReactElement {
-  const isAtleastSm = useAtleastSmall()
-  const isAtleastMd = useAtleastMedium()
-  const renderBasedOnMedia = () => {
-    if (isAtleastMd) return "MD"
-    if (isAtleastSm) return "SM"
-    return "XS"
-  }
-  return <div>{renderBasedOnMedia()}</div>
+interface Props {}
+function Navbar(props: Props): ReactElement {
+  const isSM = useAtleastSmall()
+  const [open, setOpen] = useState(false)
+  const toggleNavbar = () => setOpen(!open)
+  useEffect(() => {
+    console.log("Navbar open ? ", open)
+  }, [open])
+  return (
+    <StyledNav className="text-xl sm:text-lg">
+      <MenuButton
+        open={open}
+        onClick={(e) => toggleNavbar()}
+        title="click to open navigation"
+      >
+        Menu
+      </MenuButton>
+      <NavLinks open={open} isSM={isSM}>
+        <NavLink>About</NavLink>
+        <NavLink>Agenda</NavLink>
+        <NavLink>Speakers</NavLink>
+        <NavLink>Challenges</NavLink>
+        <NavLink>Sponsors</NavLink>
+      </NavLinks>
+    </StyledNav>
+  )
 }
 
 export default Navbar
+
+/// Styled Components
+
+const StyledNav = styled.nav.attrs((props) => ({
+  ...props,
+  className: cls(
+    "flex flex-col py-2 sm:py-4 transition duration-150 ease-in-out",
+    props.className
+  ),
+}))``
+const NavLinks = styled.ul.attrs<{ isSM: boolean; open: boolean }>((props) => ({
+  ...props,
+  className: cls(
+    "flex flex-col w-full justify-evenly sm:w-3/4 md:w-1/2 sm:flex-row items-center sm:mx-auto p-4 sm:p-2",
+    cls({
+      hidden: !props.isSM && !props.open,
+    }),
+    props.className
+  ),
+}))``
+const NavLink = styled.li.attrs((props) => ({
+  ...props,
+  className: cls("my-2", props.className),
+}))``
+
+const MenuButton = styled.button.attrs<{ open: boolean }>((props) => ({
+  ...props,
+  className: cls(
+    "text-yellow  border-yellow border-2 rounded-md p-2 m-2 sm:hidden self-end focus:bg-yellow-lighter focus:text-nightBlue focus:outline-none",
+    {
+      "bg-yellow-lighter": props.open,
+      "text-nightBlue": props.open,
+    },
+    props.className
+  ),
+}))``
---
+++ src/pages/testNavBar.tsx
@@ -0,0 +1,19 @@
+import { Link, PageProps } from "gatsby"
+import React, { ReactElement } from "react"
+import { H1 } from "../components/typography/typography"
+import Navbar from "../components/Navbar/Navbar"
+interface Props {}
+
+function TestNavbar({}: PageProps<Props>): ReactElement {
+  return (
+    <div>
+      <header className="w-full bg-nightBlue text-white">
+        <Navbar></Navbar>
+      </header>
+
+      <H1 className="text-blue">Hello in index page</H1>
+    </div>
+  )
+}
+
+export default TestNavbar
