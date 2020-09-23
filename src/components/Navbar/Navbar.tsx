264aa0e38314af8acffcaa169d67bab191be42eb
MeridjaNassim <ha_meridja@esi.dz>
2020-09-23 12:46:25 +0100

    Fixed: Navbar not showing in desktop mode and added animations to it.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,12 @@
-import React, { ReactElement, useEffect, useState } from "react"
+import React, {
+  ReactElement,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from "react"
 import styled from "styled-components"
 import cls from "classnames"
-import { useAtleastSmall } from "../../utils/medias"
+import { SMALL, useAtleastSmall } from "../../utils/medias"
 import {
   INDEX_ABOUT_SECTION,
   INDEX_AGENDA_SECTION,
@@ -35,8 +40,18 @@ const links = [
 ]
 interface Props {}
 function Navbar(props: Props): ReactElement {
+  /// when true it means the screen is atleast 640px wide
   const isSM = useAtleastSmall()
   const [open, setOpen] = useState(false)
+  /// If it detects that the device is small screen device, it sets the navbar to closed
+  useLayoutEffect(() => {
+    if (!isSM) setOpen(false)
+  }, [])
+  /// When screen size changes it closes or opens the navbar accordingly
+  useLayoutEffect(() => {
+    if (!isSM && open) return setOpen(false)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSM])
   const toggleNavbar = () => setOpen(!open)
   const renderLinks = () => {
     return links.map((link) => (
@@ -56,9 +71,7 @@ function Navbar(props: Props): ReactElement {
       >
         Menu
       </MenuButton>
-      <NavLinks open={open} isSM={isSM}>
-        {renderLinks()}
-      </NavLinks>
+      <NavLinks open={open}>{renderLinks()}</NavLinks>
     </StyledNav>
   )
 }
@@ -69,21 +82,38 @@ export default Navbar
 
 const StyledNav = styled.nav.attrs((props) => ({
   ...props,
-  className: cls(
-    "flex flex-col py-2 sm:py-4 transition duration-150 ease-in-out",
-    props.className
-  ),
+  className: cls("flex flex-col py-2 sm:py-4", props.className),
 }))``
-const NavLinks = styled.ul.attrs<{ isSM: boolean; open: boolean }>((props) => ({
-  ...props,
-  className: cls(
-    "flex flex-col w-full justify-evenly sm:w-3/4 lg:w-1/2 sm:flex-row items-center sm:mx-auto p-4 sm:p-2",
-    cls({
-      hidden: !props.isSM && !props.open,
-    }),
-    props.className
-  ),
-}))``
+const NavLinks = styled.ul.attrs<{ open: boolean }>((props) => {
+  const { open } = props
+  return {
+    ...props,
+    className: cls(
+      "flex flex-col w-full justify-evenly sm:w-3/4 lg:w-1/2 sm:flex-row items-center sm:mx-auto p-4 transition-all duration-500 linear",
+      cls({
+        collapsed: !open,
+        open: open,
+      }),
+      props.className
+    ),
+  }
+})`
+  &.collapsed {
+    max-height: 0px;
+    overflow-y: hidden;
+    padding: 0;
+  }
+  &.open {
+    max-height: 100vh;
+    overflow-y: visible;
+  }
+  @media (min-width: 640px) {
+    &.collapsed {
+      max-height: 100vh;
+      overflow-y: visible;
+    }
+  }
+`
 const NavLink = styled.li.attrs((props) => ({
   ...props,
   className: cls("my-2", props.className),
@@ -92,7 +122,7 @@ const NavLink = styled.li.attrs((props) => ({
 const MenuButton = styled.button.attrs<{ open: boolean }>((props) => ({
   ...props,
   className: cls(
-    "text-yellow  border-yellow border-2 rounded-md p-2 m-2 sm:hidden self-end focus:bg-yellow-lighter focus:text-nightBlue focus:outline-none",
+    "text-yellow transition-all duration-500 ease border-yellow border-2 rounded-md p-2 m-2 sm:hidden self-end focus:outline-none",
     {
       "bg-yellow-lighter": props.open,
       "text-nightBlue": props.open,
