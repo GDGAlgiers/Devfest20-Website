e257c8d06b867c5e25e23fc77217cb649e9262ed
MeridjaNassim <ha_meridja@esi.dz>
2020-09-25 18:18:50 +0100

    Added: Devfest Logo to Header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,11 +3,13 @@ import styled from "styled-components"
 import cls from "classnames"
 import Navbar from "../components/Navbar/Navbar"
 import { customShadow } from "../components/common/css"
+import { LARGE } from "../utils/medias"
 interface HeaderProps {}
 
 function Header(props: HeaderProps): ReactElement {
   return (
-    <StyledHeader className="z-10">
+    <StyledHeader className="z-10 relative">
+      <Brand src={require("../assets/svgs/devfest.svg")}></Brand>
       <Navbar />
     </StyledHeader>
   )
@@ -22,4 +24,13 @@ const StyledHeader = styled.header.attrs((props) => ({
   className: cls("fixed w-full bg-nightBlue text-white justify-center"),
 }))`
   ${customShadow}
+`
+const Brand = styled.img.attrs((props) => ({
+  className: "flex-col z-10 absolute self-stretch justify-center",
+}))`
+  top: 1.4rem;
+  left: 2%;
+  @media ${LARGE} {
+    top: 25%;
+  }
 `
