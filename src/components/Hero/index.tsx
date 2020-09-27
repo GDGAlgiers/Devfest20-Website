692638f4b1bf5d9032562cfcc8898a8e00b62975
akram09 <a.boutouchent@esi-sba.dz>
2020-09-27 20:28:13 +0100

    Hero Section - finished devfest card

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,21 @@
 import React, { ReactElement } from "react"
 import HeroCard from "./HeroCard"
+import styled from "styled-components"
 import Section, { WithDotsBackgroundContainer } from "../common/layout/Section"
 
 function heroSection(): ReactElement {
   return (
     <Section id="hero" className="bg-nightBlue">
       <WithDotsBackgroundContainer>
-        <div>
-          <HeroCard></HeroCard>
+        <div className="w-full flex flex-row justify-center">
+          <div className=" text-white bg-nightBlue self-stretch mr-64 ">
+            <img
+              src={require("../../assets/svgs/globe.svg")}
+              alt="Glob"
+              className="w-24"
+            />
+          </div>
+          <HeroCard containerClassName="mt-32 mr-64"></HeroCard>
         </div>
       </WithDotsBackgroundContainer>
     </Section>
