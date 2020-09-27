b96a5d7bd0a0a052cc99e950e55840eccf2b013d
akram09 <a.boutouchent@esi-sba.dz>
2020-09-27 13:33:36 +0100

    Hero Section - added gdg logo and base files

---
+++ src/components/Hero/HeroCard/index.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from "react"
+import Card from "../../common/Card/Card"
+
+interface HeroCardProps {}
+
+function HeroCard(): ReactElement {
+  return (
+    <Card>
+      <div>
+        <p>test</p>
+      </div>
+    </Card>
+  )
+}
+
+export default HeroCard
