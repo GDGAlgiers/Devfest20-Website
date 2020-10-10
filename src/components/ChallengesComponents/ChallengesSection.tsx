38be8f63c277f8a608d9eab3fa3dfcf302d2d4e5
CerineBnsd <hc_bensadi@esi.dz>
2020-10-10 21:30:01 +0100

    dinosaur fixed

--- src/components/ChallengesComponents/ChallengesSection.tsx
+++ src/components/ChallengesComponents/ChallengesSection.tsx
@@ -10,7 +10,6 @@ interface Props {}
 function ChallengesSection(props: Props): ReactElement {
   return (
     <Section id="challenges" className="bg-nightBlue">
-      <ThickBorder className="bg-red"></ThickBorder>
       <div>
         <div className="flex flex-col w-full  m-auto px-6 py-4 md:flex-row md:w-7/8 sm:px-6 sm:py-2 lg:px-20">
           <div className="p-4 lg:mt-4 lg:ml-6 bg-nightBlue">
@@ -48,7 +47,7 @@ function ChallengesSection(props: Props): ReactElement {
           }}
         >
           <img
-            src={require("../../assets/pngs/dinausor.png")}
+            src={require("../../assets/pngs/dinosor1.png")}
             alt="globe"
             className="max-h-full max-w-full"
           />
